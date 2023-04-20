25f64f011e6e36df77176943ace20d936149da67
Sebvaldesp <127505406+Sebvaldesp@users.noreply.github.com>
2023-04-20 21:51:28 +0000

    Add files via upload

---
+++ Prueba 2 ITEM II.py
@@ -0,0 +1,16 @@
+import math
+
+# Definir los radios
+radio1 = 3
+radio2 = 8
+radio3 = 10
+
+# Calcular las circunferencias
+circ1 = 2*math.pi*radio1
+circ2 = 2*math.pi*radio2
+circ3 = 2*math.pi*radio3
+
+# Imprimir los resultados
+print("La circunferencia con radio", radio1, "es:", round(circ1, 6))
+print("La circunferencia con radio", radio2, "es:", round(circ2, 6))
+print("La circunferencia con radio", radio3, "es:", round(circ3, 6))
